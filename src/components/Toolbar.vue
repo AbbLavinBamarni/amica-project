6fbf16caf755f15814555ee8e72cacf9e06b5a8e
AbbLavinBamarni <lavin.bamarni@abbindustrigymnasium.se>
2021-12-10 14:06:43 +0100

    2

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+
+  <div>
+    <v-toolbar>
+      <v-icon>mdi-account</v-icon>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
+import Icon from '@mdi/js'
 import Vue from 'vue'
 import Vuex from 'vuex'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    Icon
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+
   }
 })
